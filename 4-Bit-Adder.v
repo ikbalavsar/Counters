2639230b5a9df3f47e1a337d8b541ce20109e8ff
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2022-03-06 22:17:41 +0300

    Update 4-Bit-Adder.v

--- 4-Bit-Adder.v
+++ 4-Bit-Adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 18.08.2021 11:39:37
-// Design Name: 
-// Module Name: Lab2_Part1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Lab2_Part1(
     input [8:0] SW,
