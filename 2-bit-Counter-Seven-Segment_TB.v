ad71463e50ed46a2b65030184fc092afecc7135d
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2022-03-06 22:16:57 +0300

    Update 2-bit-Counter-Seven-Segment_TB.v

--- 2-bit-Counter-Seven-Segment_TB.v
+++ 2-bit-Counter-Seven-Segment_TB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2021 15:13:35
-// Design Name: 
-// Module Name: Lab4_Part4_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Lab4_Part4_TB();
 
