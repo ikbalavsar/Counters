62b0c28747656360444f3c736780c9bdceae6021
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2022-03-06 22:19:57 +0300

    Update 8-bit-Synchronous-Counter_TB.v

--- 8-bit-Synchronous-Counter_TB.v
+++ 8-bit-Synchronous-Counter_TB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.08.2021 10:55:40
-// Design Name: 
-// Module Name: Lab4_Part1_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Lab4_Part1_TB();
 
