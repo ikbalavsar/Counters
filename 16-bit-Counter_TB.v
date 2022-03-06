60941b688cd3a2153a920caed1d8e7eaeec00d8a
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2022-03-06 22:16:22 +0300

    Update 16-bit-Counter_TB.v

--- 16-bit-Counter_TB.v
+++ 16-bit-Counter_TB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2021 08:55:09
-// Design Name: 
-// Module Name: Lab4_Part2_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Lab4_Part2_TB();
     reg CLK100MHZ_TB  = 0;
