1ee06f083740e8a5dadb4555c185dfca9cfdb0dc
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2022-03-06 22:17:25 +0300

    Update 3-bit-Counter-Character-Shifting_TB.v

--- 3-bit-Counter-Character-Shifting_TB.v
+++ 3-bit-Counter-Character-Shifting_TB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.09.2021 14:02:51
-// Design Name: 
-// Module Name: Lab4_Part5_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Lab4_Part5_TB();
 
