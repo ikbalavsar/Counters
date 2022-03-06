7e2c744678c686a50e1d75df27434dbd52e23f3b
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2022-03-06 22:19:04 +0300

    Update 8-Bit-Adder.v

--- 8-Bit-Adder.v
+++ 8-Bit-Adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.08.2021 08:05:00
-// Design Name: 
-// Module Name: Lab3_Part5_2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Lab3_Part5_2(SW,CLK100MHZ,LEDR, HEX0, AN);
     input [9:0] SW;
